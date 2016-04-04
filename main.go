b10f8c2e71413d561f9ba27179da27dd1e054f1d
Rob Cameron <rwcameron@gmail.com>
2016-04-03 19:20:44 -0700

    being bothered, need to stop

--- main.go
+++ main.go
@@ -1,22 +1,66 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
+	"time"
 
-	git2go "github.com/libgit2/git2go"
+	git2go "gopkg.in/libgit2/git2go.v22"
 )
 
 func main() {
 
-	r, err := git2go.InitRepository("./test_repo", false)
+	var r *git2go.Repository
+	var err error
+
+	repo_log := "./test_repo"
+
+	r, err = git2go.OpenRepository(repo_log)
+	if err != nil {
+
+		r, err = git2go.InitRepository(repo_log, false)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	idx, err := r.Index()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	c, err := r.Config()
+	err = ioutil.WriteFile(repo_log+"/happy.txt", []byte(time.Now().String()+"\n"), os.ModePerm)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(c)
+	err = idx.AddByPath("happy.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	idx_id, err := idx.WriteTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	o, err := r.Lookup(idx_id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tree, err := *o.AsTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sig := &git2go.Signature{Name: "Rob Cameron", Email: "rwcameron@gmail.com", When: time.Now()}
+
+	id, err := r.CreateCommit("HEAD", sig, sig, "Updating happiness", tree, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(id)
 }
